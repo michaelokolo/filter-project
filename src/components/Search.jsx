d3c6afe315ca155693177e7c0554f8c24d439945
Mike <michaelokolo62@yahoo.com>
2024-02-25 02:14:13 +0000

    added the on and off state for the extract data and monitoring buttons

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,42 @@
-import { Button, Dropdown, TextInput } from 'flowbite-react';
 import React, { useState } from 'react';
 import { HiArrowsUpDown } from 'react-icons/hi2';
 import { MdMonitor } from 'react-icons/md';
-import { FaPlus } from 'react-icons/fa6';
 import FilterBySitesBtn from './FilterBySitesBtn';
 import FilterByCategoriesBtn from './FilterByCategoriesBtn';
 
 export default function Search({ items }) {
+  const [extractData, setExtractData] = useState(false);
+  const [monitoring, setMonitoring] = useState(false);
+
+  const handleExtractData = () => {
+    setExtractData(!extractData);
+  };
+  const handleMonitoring = () => {
+    setMonitoring(!monitoring);
+  };
+
   return (
     <div className="">
       <form className="flex gap-2 flex-wrap">
         <button
-          className="text-purple-700 border 
-        bg-purple-50 border-purple-700 rounded-full 
-        px-4 py-1 flex items-center gap-2"
+          type="button"
+          className={
+            extractData
+              ? 'text-purple-700 border bg-purple-50 border-purple-700 rounded-full px-4 py-1 flex items-center gap-2'
+              : 'text-gray-500 border bg-white border-gray-500 rounded-full px-4 py-1 flex items-center gap-2'
+          }
+          onClick={handleExtractData}
         >
           <HiArrowsUpDown /> <span>Extract Data</span>
         </button>
         <button
-          className="text-purple-700 border bg-purple-50
-         border-purple-700 rounded-full px-4 py-1 flex items-center gap-2"
+          type="button"
+          className={
+            monitoring
+              ? 'text-purple-700 border bg-purple-50 border-purple-700 rounded-full px-4 py-1 flex items-center gap-2'
+              : 'text-gray-500 border bg-white border-gray-500 rounded-full px-4 py-1 flex items-center gap-2'
+          }
+          onClick={handleMonitoring}
         >
           <MdMonitor /> <span>Monitoring</span>
         </button>
